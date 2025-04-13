d2326c032e09fa1f7a2a8bc459ff140b217dce18
SMIT MORJARIYA <er.smit.2004@gmail.com>
2025-04-13 13:29:07 +0530

    update Login.jsx

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+
+  const navigate = useNavigate();
+
+
+  // handle login form submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+
   return (
     <AuthLayout>
-      
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black ">Welcome Back </h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6 ">
+          Please Enter your details to log in
+        </p>
+      </div>
+
     </AuthLayout>
   )
 }
