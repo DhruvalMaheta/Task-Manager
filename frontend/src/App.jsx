cee6e0c138cf43a085c82557c0def58e2ecef54a
SMIT MORJARIYA <er.smit.2004@gmail.com>
2025-04-14 22:09:57 +0530

    Create a Login & SignUp Pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from './pages/Admin/Dashboard.jsx';
+import Login from './pages/Auth/Login.jsx';
+import SignUp from './pages/Auth/SignUp.jsx';
+import ManageTasks from './pages/Admin/ManageTasks.jsx';
+import Createtask from './pages/Admin/Createtask.jsx';
+import ManageUser from './pages/Admin/ManageUser.jsx';
+
+import UserDashboard from './pages/User/UserDashboard.jsx';
+import MyTasks from './pages/User/MyTasks.jsx';
+import ViewTaskDetails from './pages/User/ViewTaskDetails.jsx';
+
+import PrivateRoute from './routes/PrivateRoutes.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={< SignUp />} />
+
+          {/* Admin Route */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path='/admin/dashboard' element={<Dashboard />} />
+            <Route path='/admin/tasks' element={<ManageTasks />} />
+            <Route path='/admin/create-tasks' element={<Createtask />} />
+            <Route path='/admin/users' element={<ManageUser />} />
+          </Route>
+
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path='/user/dashboard' element={<UserDashboard />} />
+            <Route path='/user/tasks' element={<MyTasks />} />
+            <Route path='/user/task-details/:id' element={<ViewTaskDetails />} />
+          </Route>
+
+        </Routes>
+      </Router>
+    </div>
+
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import Input from '../../components/Inputs/Input';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
+
+
+  //Handle SignUp Form Submit
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter Full Name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+
+    setError("");
+
+
+
+    // SignUp API Call 
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account </h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <span className='flex flex-col justify-center text-center mb-4'>
+            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />Upload Profile Photo</span>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John Dude"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+              required
+            />
+
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 Characters"
+              type="password"
+              required
+            />
+
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Admin Invite Token"
+              placeholder="6 Digit Code"
+              type="text"
+              required
+            />
+
+          </div>
+          {error && <p className="text-red-500 text-xs mt-2">{error}</p>}
+
+
+          <button type="submit" className="btn-primary">
+            SIGN UP
+          </button>
+
+          <p className="text-[15px] text-slate-800 mt-3">
+            Have an account? {" "}
+            <Link className="font-semibold text-blue-500 underline" to="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  )
+}
+
+export default SignUp
