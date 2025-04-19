a192405d9399eb95edb646e8b9e1564d1517d3e8
SMIT MORJARIYA <er.smit.2004@gmail.com>
2025-04-19 22:55:37 +0530

    Update server.js  file

--- server/server.js
+++ server/server.js
@@ -45,5 +45,5 @@ app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port : ${PORT}`);
 });
